a2bef29c6edaa8effb321338f02911d020aa2150
Nguyen Duc Tu <87971083+Hyena82@users.noreply.github.com>
2024-12-09 22:40:01 +0700

    feat: add login endpoint

--- src/services/access.service.js
+++ src/services/access.service.js
@@ -8,6 +8,7 @@ const {
   BadRequestError,
   ConflictRequestError,
 } = require("../core/error.response");
+const { findByEmail } = require("./shop.service");
 
 const ROLESHOP = {
   SHOP: "SHOP",
@@ -17,13 +18,44 @@ const ROLESHOP = {
 };
 
 class AccessService {
+  static login = async ({ email, password, refreshToken = null }) => {
+    const foundShop = await findByEmail({ email });
+    if (!foundShop) {
+      throw new BadRequestError("Shop not registered");
+    }
+    const match = bcrypt.compare(password, foundShop.password);
+    if (!match) {
+      throw BadRequestError("Password is not correct");
+    }
+
+    const privateKey = crypto.randomBytes(64).toString("hex");
+    const publicKey = crypto.randomBytes(64).toString("hex");
+    const { _id: userId } = foundShop;
+    const tokens = await createTokenPair(
+      { userId, email },
+      publicKey,
+      privateKey
+    );
+
+    await KeyTokenService.createKeyToken({
+      userId,
+      refreshToken: tokens.refreshToken,
+      privateKey,
+      publicKey,
+    });
+
+    return {
+      shop: getIntoData(["name", "email"], foundShop),
+      tokens,
+    };
+  };
+
   static signUp = async ({ name, email, password }) => {
     try {
       const holderShop = await shopModel.findOne({ email }).lean();
       if (holderShop) {
         throw new BadRequestError("Error: Shop already registered!");
       }
-
       const passwordHash = await bcrypt.hash(password, 10);
 
       const newShop = await shopModel.create({
--- src/services/keyToken.js
+++ src/services/keyToken.js
@@ -1,13 +1,30 @@
 const keysModel = require("../models/keys.model");
 
 class KeyTokenService {
-  static createKeyToken = async ({ userId, publicKey, privateKey }) => {
+  static createKeyToken = async ({
+    userId,
+    publicKey,
+    privateKey,
+    refreshToken,
+  }) => {
     try {
-      const tokens = await keysModel.create({
-        user: userId,
-        publicKey,
-        privateKey,
-      });
+      // const tokens = await keysModel.create({
+      //   user: userId,
+      //   publicKey,
+      //   privateKey,
+      // });
+
+      // return tokens ? tokens.publicKey : null;
+      const filter = { user: userId },
+        update = {
+          publicKey,
+          privateKey,
+          refreshTokensUsed: [],
+          refreshToken,
+        },
+        options = { upsert: true, new: true };
+
+      const tokens = await keysModel.findOneAndUpdate(filter, update, options);
 
       return tokens ? tokens.publicKey : null;
     } catch (e) {
